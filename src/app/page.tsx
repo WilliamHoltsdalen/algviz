2816e5296bebf9f53fdc2d86a1fc7f04db0f0957
WilliamHoltsdalen <williamlh@hotmail.no>
2025-09-03 11:50:07 +0200

    feat(nav): make navbar for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import FeatureSection from '@/components/FeatureSection';
 import AlgorithmsShowcase from '@/components/AlgorithmsShowcase';
 import BottomCTA from '@/components/BottomCTA';
 import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <LandingHero />
       <FeatureSection />
       <AlgorithmsShowcase />
